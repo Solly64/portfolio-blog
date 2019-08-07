8ac9037f0cc6086e8c829b54c8704697c9149a73
git@github.com:Solly64/datasciencecoursera.git <git@github.com:Solly64/datasciencecoursera.git>
2019-08-07 13:53:49 -0400

    changes

--- portfolio1/settings.py
+++ portfolio1/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb1',
         'USER': 'postgres',
-        'PASSWORD': 'abcdefg',
+        'PASSWORD': 'Oregon2726!blue4341',
         'HOST': 'localhost',
         'PORT': '5432',
     }
